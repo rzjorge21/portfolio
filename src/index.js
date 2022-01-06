757c8ca5de8bda14d2e8dbcc7d6e43c3a8fa093b
Jorge Ramón <rz.jorge21@gmail.com>
2022-01-06 18:57:08 -0500

    router fixed

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
